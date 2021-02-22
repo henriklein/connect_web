5367e5b81a699ab847b2c6847ac531e1badc4b77
emilio <emoxic.sama@gmail.com>
2021-02-22 10:08:35 -0600

    postprocessng_m

--- postprocessing.js
+++ postprocessing.js
@@ -1,13 +1,16 @@
 
 function reposition_button(){
 	var wraps = document.getElementsByClassName("rs-parallax-wrap"); 
+	var target_inside = document.getElementsByClassName("rs-parallax-wrap").item(10).children[0].children[0]
 	var target = wraps.item(10);
 
 	if (window.innerWidth < 768){
 		target.classList.add('center-button');
+		target_inside.classList.add('center-button');
 		console.log('added');
 	}else{
 		console.log('removed');
+		target_inside.classList.remove('center-button');
 		target.classList.remove('center-button');
 	}
 }
